ff7dfaa39b363dad3fd63444ffa254b8793ba69f
14storm <rehobothsadiku@gmail.com>
2026-09-11 09:18:31 +0100

    comment out HowItWorks section in LandingPage

--- src/App.jsx
+++ src/App.jsx
@@ -24,7 +24,7 @@ function LandingPage() {
       <Features />
       <Security />
       <Cards />
-      <HowItWorks />
+      {/* <HowItWorks /> */}
       <BrandStory />
       <FinalCta />
       <Footer />
